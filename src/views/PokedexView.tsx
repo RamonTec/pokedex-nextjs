2c7cdf2c7ad19d59befb23fe4b8d7fde5591cc91
Elias Ramon <eestrabao46@gmail.com>
2025-11-02 22:51:29 -0400

    feat: pokdex view to render list of pokemones

---
+++ src/views/PokedexView.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import PokemonList from "../components/PokemonsList";
+
+const PokedexView = () => {
+  return (
+    <>
+      <p>Pokemons list</p>
+      <PokemonList />
+    </>
+  )
+}
+
+export default PokedexView;
