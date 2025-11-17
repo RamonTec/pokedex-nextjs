68a2b5f62a9871a02abe59057d25d73164a0c7a2
Elias Ramon <eestrabao46@gmail.com>
2025-11-17 12:59:04 -0400

    feat: integrated library react infinite scroll

--- src/components/PokemonsList.tsx
+++ src/components/PokemonsList.tsx
@@ -1,9 +1,10 @@
 
+import InfiniteScroll from "react-infinite-scroll-component";
 import { usePokedexList } from "../hooks/usePokedex";
 import PokemonCard from "./PokemonCard";
 
 const PokemonList = () => {
-  const { pokedexData: pokemons, loading, error } = usePokedexList();
+  const { pokedexData: pokemons, loading, error, handleFetchMorePokemons } = usePokedexList();
 
   if (loading) {
     return (
@@ -25,18 +26,31 @@ const PokemonList = () => {
 
   return (
     <section className="mx-auto max-w-7xl py-8">
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-        {pokemons.map((pokemon) => {
-          const id = pokemon.url.split("/").slice(-2, -1)[0];
-          return (
-            <PokemonCard
-              key={pokemon.name}
-              id={id}
-              name={pokemon.name}
-            />
-          );
-        })}
-      </div>
+      <InfiniteScroll
+        dataLength={pokemons?.results.length || 0}
+        next={handleFetchMorePokemons}
+        hasMore={pokemons?.next !== null}
+        loader={<h4>Loading...</h4>}
+        scrollThreshold={1}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+          {pokemons?.results.map((pokemon) => {
+            const id = pokemon.url.split("/").slice(-2, -1)[0];
+            return (
+              <PokemonCard
+                key={pokemon.name}
+                id={id}
+                name={pokemon.name}
+              />
+            );
+          })}
+        </div>
+      </InfiniteScroll>
     </section>
   );
 };
